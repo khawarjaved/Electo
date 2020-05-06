1e2e3c8ff742c5e442889d205dcc9c29a4aacafc
khawa <khavarjk@gmail.com>
2020-05-06 19:32:21 +0500

    Initial commit

---
+++ app/src/main/java/developer/jk/electo/MainActivity.java
@@ -0,0 +1,14 @@
+package developer.jk.electo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
